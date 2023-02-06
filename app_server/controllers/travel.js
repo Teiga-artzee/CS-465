029b608536ef99c9bf0b8d4074c028696a4f4b27
Alexandrea Fullwood <alexandrea.teigeler@snhu.edu>
2023-02-05 20:27:11 -0600

    Uncomment code that prevented application from launching

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -50,10 +50,10 @@ const travelList = (req, res) => {
 };
 
 /* Get travel view */
-//const travel = (req, res) => {
-//    res.render('travel', { title: 'Travlr Getaways', trips});
-//};
+const travel = (req, res) => {
+    res.render('travel', { title: 'Travlr Getaways', trips});
+};
 
-//module.exports = {
-//    travel
-//};
+module.exports = {
+    travel
+};
